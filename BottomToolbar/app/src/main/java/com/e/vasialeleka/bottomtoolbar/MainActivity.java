7fb29373cb6f2f2dab91e763db3366bd4198c9bc
Vasia <vasialeleka@ukr.net>
2018-12-04 10:39:20 +0200

    BottomToolbar

---
+++ BottomToolbar/app/src/main/java/com/e/vasialeleka/bottomtoolbar/MainActivity.java
@@ -0,0 +1,48 @@
+package com.e.vasialeleka.bottomtoolbar;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                   selectedFragment = new HomeFragment();
+                    break;
+                case R.id.navigation_dashboard:
+                    selectedFragment = new FavoriteFragment();
+                    break;
+                case R.id.navigation_notifications:
+                    selectedFragment = new SearchFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_conteiner,selectedFragment).commit();
+            return true;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_conteiner,new HomeFragment()).commit();
+    }
+
+}
