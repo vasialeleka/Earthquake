4bdf5c30e0a170d648459913cde067b2b16dcf05
Vasia <vasialeleka@ukr.net>
2019-01-16 12:11:49 +0200

    swipe refresh

--- Java/RetrofitApps/NewsApp/app/src/main/java/com/e/vasialeleka/newsapp/NewsActivity.java
+++ Java/RetrofitApps/NewsApp/app/src/main/java/com/e/vasialeleka/newsapp/NewsActivity.java
@@ -2,6 +2,7 @@ package com.e.vasialeleka.newsapp;
 
 
 import android.content.Context;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -29,84 +30,113 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class NewsActivity extends AppCompatActivity {
-public static  final String API_KEY="e60697f7f9874f35a82708c0df1618a9";
-private RecyclerView recyclerView;
-private  RecyclerView.LayoutManager layoutManager;
-private List<Article> articles = new ArrayList<>();
-private  Adapter adapter;
+    public static final String API_KEY = "e60697f7f9874f35a82708c0df1618a9";
+    private RecyclerView recyclerView;
+    private RecyclerView.LayoutManager layoutManager;
+    private List<Article> articles = new ArrayList<>();
+    private Adapter adapter;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+        swipeRefreshLayout = findViewById(R.id.swipe_refresh);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                loadJson("");
+                Toast.makeText(NewsActivity.this, "Refresh", Toast.LENGTH_SHORT).show();
+            }
+        });
+
         recyclerView = findViewById(R.id.recyclrView);
         layoutManager = new LinearLayoutManager(NewsActivity.this);
         recyclerView.setLayoutManager(layoutManager);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setNestedScrollingEnabled(false);
-        loadJson("");
+
+        onLoadingSwipeRefresh("");
 
     }
 
-    public void loadJson(final String keyword){
-
+    public void loadJson(final String keyword) {
+swipeRefreshLayout.setRefreshing(true);
         ApiInterface apiInterface = ApiClient.getApiClient().create(ApiInterface.class);
         //Call<News> call;
         String country = Utils.getCountry();
         retrofit2.Call<News> call;
 
-        if(keyword.length()>0){
-            call = apiInterface.getNewsSearch(keyword,"publishedAt",API_KEY);
-        }else{
-         call = apiInterface.getNews(country,API_KEY);}
-         call.enqueue(new Callback<News>() {
-             @Override
-             public void onResponse(retrofit2.Call<News> call, Response<News> response) {
-                 if (response.isSuccessful()&& response.body().getArticle()!=null){
-                     if(!articles.isEmpty()){articles.clear();}
-                     articles= response.body().getArticle();
-                     adapter = new Adapter(articles,NewsActivity.this);
-                     recyclerView.setAdapter(adapter);
-                     adapter.notifyDataSetChanged();
+        if (keyword.length() > 0) {
+            call = apiInterface.getNewsSearch(keyword, "publishedAt", API_KEY);
+        } else {
+            call = apiInterface.getNews(country, API_KEY);
+        }
+        call.enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(retrofit2.Call<News> call, Response<News> response) {
+                if (response.isSuccessful() && response.body().getArticle() != null) {
+                    if (!articles.isEmpty()) {
+                        articles.clear();
+                    }
+                    articles = response.body().getArticle();
+                    adapter = new Adapter(articles, NewsActivity.this);
+                    recyclerView.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                    swipeRefreshLayout.setRefreshing(false);
 
-                 }else{
-                     Toast.makeText(NewsActivity.this,"No results",Toast.LENGTH_SHORT).show();
-                 }
-             }
+                } else {
+                    swipeRefreshLayout.setRefreshing(false);
 
-             @Override
-             public void onFailure(retrofit2.Call<News> call, Throwable t) {
+                    Toast.makeText(NewsActivity.this, "No results", Toast.LENGTH_SHORT).show();
+                }
+            }
 
-             }
-         });
+            @Override
+            public void onFailure(retrofit2.Call<News> call, Throwable t) {
+                swipeRefreshLayout.setRefreshing(false);
+
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main,menu);
+        inflater.inflate(R.menu.menu_main, menu);
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         final SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
-        MenuItem seachMenuItems =  menu.findItem(R.id.action_search);
+        MenuItem seachMenuItems = menu.findItem(R.id.action_search);
         searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
         searchView.setQueryHint("Search News...");
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-             if(query.length()>2){
-                 loadJson(query);
-             }
+                if (query.length() > 2) {
+                    onLoadingSwipeRefresh(query);
+                }
                 return false;
             }
 
             @Override
             public boolean onQueryTextChange(String newText) {
-                loadJson(newText);
+                onLoadingSwipeRefresh(newText);
                 return false;
             }
         });
-seachMenuItems.getIcon().setVisible(false,false);
-       return true;
+        seachMenuItems.getIcon().setVisible(false, false);
+        return true;
         //return super.onCreateOptionsMenu(menu);
+    }
+
+    private void onLoadingSwipeRefresh(final String keyword) {
+        swipeRefreshLayout.post(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        loadJson(keyword);
+                    }
+                }
+        );
     }
 }
